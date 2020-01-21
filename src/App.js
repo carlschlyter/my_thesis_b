8c17fa42470fba3fa48453f6bd693d1af7dc83ab
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-01-21 15:04:52 +0100

    turned app.js into class,  added axios and connected to test api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import StartPage from './components/StartPage';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <StartPage />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    choices: []
+  }
+
+componentDidMount(){
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+  .then(res => this.setState({ choices: res.data}))
+}
+
+  render() {
+    return (
+      <div className="App">
+        <StartPage />
+      </div>
+    );
+  }
 }
 
 export default App;
