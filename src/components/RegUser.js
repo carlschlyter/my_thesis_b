60e6099e8915bfa1c90e6fd66e0939aa38a70890
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-01-28 10:12:15 +0100

    added RegUser component file

---
+++ src/components/RegUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class RegUser extends Component {
+    render() {
+        return (
+            <form>
+
+            </form>
+        )
+    }
+}
+
+export default RegUser
