73f3d43ff895f0d3589dc9142f77b4c2e759459d
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-01-28 09:04:14 +0100

    general adjustments

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 export default class StartPage extends Component {
     render() {
-
         return (
             <div>
                 <h1>Guessing App For Advertising Agencies</h1>
