27d48f4bd450ffb511933f88cc189ac74fd78fbc
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-02-06 09:09:37 +0100

    only minor layout changes

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class StartPage extends Component {
     render() {
         return (
-            <div>
+            <div className="startPage">
                 <h1>Guessing App For Advertising Agencies</h1>
                 <h2>Activate your customers' target groups with a guessing game!</h2><br/>
                 <h3>Current games to make guesses on: </h3><br/>
